0b8c3ad95b1854ab91b902a654a941bad9e1bbcd
Yue Yang <this.isssssyy@gmail.com>
2017-04-24 14:39:54 +0800

    Lab5 exercise 1 completed. set IOPL bit to eflags for file env.

--- kern/env.c
+++ kern/env.c
@@ -410,7 +410,9 @@ env_create(uint8_t *binary, enum EnvType type)
 	load_icode(new_env, binary);
 
 	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
-	// LAB 5: Your code here.
+	if (type == ENV_TYPE_FS) {
+		new_env->env_tf.tf_eflags |= FL_IOPL_MASK;
+	}
 }
 
 //
